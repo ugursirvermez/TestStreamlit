97fe97fbc1bd322a0d5f7a80144eb335ff9deb51
Uğur Sırvermez <ugursirvermez@hotmail.com>
2026-06-05 11:36:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -49,5 +49,25 @@ with col_btn:
 st.write("")
 
 #GÖREVLERİN LİSTESİ
+if not st.session_state.tasks:
+    st.info("Burası şuan boş!")
 
-#ÖZET
+for i, task in enumerate(st.session_state.tasks):
+    c1,c2,c3 = st.columns([0.5,5, 0.8])
+    with c1:
+        checked =st.checkbox("",value= task["done"], key=f"chk_{i}",
+                             label_visibility="collapsed") #Label kapalı
+        if checked != task["done"]:
+            st.session_state.tasks[i]["done"] = checked
+            st.rerun()
+    with c2:
+        st.markdown(
+            f"""
+            <div>{task["text"]}</div>
+            """,
+            unsafe_allow_html=True
+        )
+    with c3:
+        if st.button("X", key=f"del_{i}"):
+            st.session_state.tasks.pop(i)
+            st.rerun()
