bd8ba3d28d549eb1afb3f3d879e989f360bb1add
Uğur Sırvermez <ugursirvermez@hotmail.com>
2026-06-05 11:22:35 +0300

    SecondUpdate

--- app.py
+++ app.py
@@ -1,20 +1,53 @@
 import streamlit as st #Streamlit
 #pip install streamlit
+import pandas as pd
 
-st.title("Merhaba")
+#Sayfa configure ayarı yapılır. Bu sayfanın temel özelliklerini belirtir.
+st.set_page_config(page_title="Mini Görev Panosu", 
+                   page_icon="pictures/checklist.png", layout="centered")
 
-if st.button("Tikla"):
-    st.write("Merhaba, Streamlit")
-else:
-    st.write("Butona bas")
+#STYLE -> CSS KISMI
+st.markdown(
+"""
+<style>
+.app-header{
+    color: blue;
+    padding: 28px, 32px;
+}
+</style>
+""",
+unsafe_allow_html=True
 
-#Veri alma
-kilo = st.number_input("Kilo (kg)", min_value=0.0)
-yas = st.number_input("Boy(cm)", min_value=0.0)
+)
+#DURUM -> Görev durumlarını belirten bir değişken
+if "tasks" not in st.session_state:
+    st.session_state.tasks = [
+        {"text": "Streamlit Hesabı Aç", "done": True},
+        {"text": "Uygulamayı Yükle", "done": True}
+    ]
+#BAŞLIK -> HTML DIV
+st.markdown(
+    """
+    <div class = "app-header"> 
+    <h1>GÖREV LİSTESİ</h1>
+    <p>TEST PROJEMİZ</p>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+#GÖREVLER EKLEME
+col_input, col_btn = st.columns([4,1])
+with col_input:
+    new_task = st.text_input("Yeni Görev", placeholder="Görevi yaz...", 
+                             label_visibility="visible")
+with col_btn:
+    if st.button("Ekle", use_container_width=True):
+        if new_task.strip():
+            st.session_state.tasks.append({"text": new_task.strip(),
+                                           "done":False})
+            st.rerun() #Güncelleme yap
+st.write("")
 
-if st.button("Hesapla"):
-    if yas > 0:
-        vk= kilo / (yas /100)
-        st.metric("Sonuc:", f"{vk}")
-    else:
-        st.error("HATA")
+#GÖREVLERİN LİSTESİ
+
+#ÖZET
